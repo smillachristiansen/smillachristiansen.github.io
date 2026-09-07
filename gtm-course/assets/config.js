2bb6c52bda232ffbc3327df4ed29e75202eb0c71
Smilla Christiansen <smillachristiansen@Smillas-MacBook-Neo.local>
2026-09-07 12:03:18 +0200

    Add GTM container ID

--- gtm-course/assets/config.js
+++ gtm-course/assets/config.js
@@ -13,9 +13,7 @@
 
 window.GTM_COURSE_CONFIG = {
 
-    containerId: '
-GTM-T462PMDD',
-GTM-T462PMDD
+    containerId: 'GTM-T462PMDD',
 
     /* Optional. Set to true to hide the "paste your container ID" box on every
        page, once you have set the ID above and do not want students changing it. */
